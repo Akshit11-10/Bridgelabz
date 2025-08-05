b51f83f63b42d2e38ce9b4c5a70d4e7eb8a97b7b
Akshit Saxena <akshatsaxena102005@gmail.com>
2025-08-05 12:07:54 +0530

    divide

--- Calci.java
+++ Calci.java
@@ -1,8 +1,11 @@
+
 public class Calci {
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Calculator");
-        int a=10;
-        int b=12;
-        System.out.println("Sum of the numbers is: " + (a+b));
+        int a = 10;
+        int b = 12;
+        System.out.println("Sum of the numbers is: " + (a + b));
+        System.out.println("divide of the numbers is: " + (a + b));
     }
 }
