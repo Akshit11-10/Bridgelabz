fc1d29f8d1436f85528f294ab777035a8f23aa70
Akshit Saxena <akshatsaxena102005@gmail.com>
2025-08-05 10:50:07 +0530

    initial commit

---
+++ Calci.java
@@ -0,0 +1,8 @@
+public class Calci {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Calculator");
+        int a=10;
+        int b=12;
+        System.out.println("Sum of the numbers is: " + (a+b));
+    }
+}
